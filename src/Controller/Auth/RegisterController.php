459714299e02db54e430184526e450b8dbc6d35c
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-09-21 08:20:44 +0200

    Register Controller 3

--- src/Controller/Auth/RegisterController.php
+++ src/Controller/Auth/RegisterController.php
@@ -4,12 +4,18 @@ namespace App\Controller\Auth;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 final class RegisterController extends AbstractController
 {
     #[Route('/api/v1/auth/register', name: 'api_auth_register', methods: ['POST'])]
-    public function __invoke(): JsonResponse
+    public function __invoke(
+        Request $request,
+        // App\Controller\Auth\SerializerInterface
+        SerializerInterface $serializer
+        ): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
