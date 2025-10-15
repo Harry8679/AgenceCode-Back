3cca19a807fca00aded6813d90fe8f0f72f20261
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-10-15 07:45:29 +0200

    Child Provider

--- src/Entity/Child.php
+++ src/Entity/Child.php
@@ -2,42 +2,73 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata as API;
 use App\Enum\ClassLevel;
 use App\Repository\ChildRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use App\Api\Provider\MyChildrenProvider;
+use App\Api\Processor\ChildOwnerProcessor;
 
 #[ORM\Entity(repositoryClass: ChildRepository::class)]
+#[API\ApiResource(
+    normalizationContext: ['groups' => ['child:read']],
+    denormalizationContext: ['groups' => ['child:write']],
+    operations: [
+        // collection GET => via provider pour filtrer par parent connecté
+        new API\GetCollection(
+            provider: MyChildrenProvider::class,
+            security: "is_granted('ROLE_PARENT')"
+        ),
+        // POST => parent only, on attribue le parent connecté dans un Processor
+        new API\Post(
+            security: "is_granted('ROLE_PARENT')",
+            processor: ChildOwnerProcessor::class
+        ),
+        // item GET => autorisé si owner
+        new API\Get(security: "is_granted('ROLE_PARENT') and object.getParent() == user"),
+        new API\Patch(security: "is_granted('ROLE_PARENT') and object.getParent() == user"),
+        new API\Delete(security: "is_granted('ROLE_PARENT') and object.getParent() == user"),
+    ]
+)]
 class Child
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['child:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['child:read','child:write'])]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['child:read','child:write'])]
     private ?string $lastName = null;
 
     #[ORM\Column(enumType: ClassLevel::class)]
+    #[Groups(['child:read','child:write'])]
     private ?ClassLevel $classLevel = null;
 
     #[ORM\Column]
+    #[Groups(['child:read'])]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\Column(nullable: true)]
+    #[Groups(['child:read'])]
     private ?\DateTimeImmutable $updatedAt = null;
 
     #[ORM\ManyToOne(inversedBy: 'children')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?User $parent = null;
+    private ?User $parent = null; // pas exposé en write
 
     /** @var Collection<int, Subject> */
     #[ORM\ManyToMany(targetEntity: Subject::class, inversedBy: 'children')]
     #[ORM\JoinTable(name: 'child_subject')]
+    #[Groups(['child:read','child:write'])]   // on poste des IDs de Subject
     private Collection $subjects;
 
     public function __construct()
