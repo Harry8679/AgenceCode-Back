005e63ae9ded8a253f8a6727585eb61293322069
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-11-14 05:26:29 +0100

    MyTeachersController 2

--- src/Controller/MyTeachersController.php
+++ src/Controller/MyTeachersController.php
@@ -1,18 +1,49 @@
 <?php
+// src/Controller/MyTeachersController.php
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Enum\UserProfile;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class MyTeachersController extends AbstractController
 {
-    #[Route('/my/teachers', name: 'app_my_teachers')]
-    public function index(): Response
+    #[Route('/api/my/teachers', methods: ['GET'])]
+    #[IsGranted('ROLE_PARENT')]
+    public function list(EntityManagerInterface $em): JsonResponse
     {
-        return $this->render('my_teachers/index.html.twig', [
-            'controller_name' => 'MyTeachersController',
-        ]);
+        /** @var User $parent */
+        $parent = $this->getUser();
+
+        $teachers = $em->createQueryBuilder()
+            ->select('DISTINCT t')
+            ->from(User::class, 't')
+            ->innerJoin('t.couponUsages', 'cu')
+            ->innerJoin('cu.coupon', 'co')
+            ->innerJoin('co.child', 'ch')
+            ->where('t.profile = :teacher')
+            ->andWhere('ch.parent = :parent')
+            ->setParameter('teacher', UserProfile::TEACHER)
+            ->setParameter('parent', $parent)
+            ->orderBy('t.lastName', 'ASC')
+            ->addOrderBy('t.firstName', 'ASC')
+            ->getQuery()
+            ->getResult();
+
+        // On sérialise “à la main” les champs autorisés
+        $data = array_map(fn(User $t) => [
+            'id'         => $t->getId(),
+            'firstName'  => $t->getFirstName(),
+            'lastName'   => $t->getLastName(),
+            'email'      => $t->getEmail(),
+            'phone'      => $t->getPhoneNumber(),
+        ], $teachers);
+
+        return $this->json($data);
     }
 }
